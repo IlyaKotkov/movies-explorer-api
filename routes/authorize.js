bad7219a62f181475c1ba7eccf8517c55a99a117
Ilya Kotkov <MopsBox2552@yandex.ru>
2023-06-14 22:09:32 +0300

    исправить ошибки

--- routes/authorize.js
+++ routes/authorize.js
@@ -13,8 +13,6 @@ router.post('/signup', celebrate({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-/]))?/),
   }),
 }), createUser);
 
