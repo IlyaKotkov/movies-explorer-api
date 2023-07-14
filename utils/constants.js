5bd00a38c4f99d866ce8ad78cfdd73a154ddb2be
Ilya Kotkov <MopsBox2552@yandex.ru>
2023-07-14 18:22:38 +0300

    исправить регулярное выражение

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-module.exports.regexLink = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
+module.exports.regexLink =  /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
 
 module.exports.FILM_INVALID_DATA = 'Переданы некорректные данные при создании фильма.';
 module.exports.FILM_NOT_FOUND = 'фильм с указанным _id не найден.';
