31a815f32a3cb2df2257cdca58aff2df1509e1f2
Ilya Kotkov <MopsBox2552@yandex.ru>
2023-07-14 19:30:10 +0300

    Исправить обновление юзера

--- utils/validation/updateInfoUserValidation.js
+++ utils/validation/updateInfoUserValidation.js
@@ -2,8 +2,7 @@ const { celebrate, Joi } = require('celebrate');
 
 module.exports.updateInfoUserValidation = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
+    email: Joi.string().required().email()
   }),
 });
